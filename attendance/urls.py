fb04edfc32912155db40203305d1854d2ac09d0c
samdasu <full17@naver.com>
2017-07-24 01:24:00 +0900

    0723

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
 	url(r'^analysis$', TemplateView.as_view(template_name='info/analysis.html'), name='analysis'),
+	url(r'^upload$', views.simple_upload, name="upload")
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 def home(request):
 	return render(request, 'info/home.html')
 
 def analysis(request):
     return render(request, 'info/analysis.html')
+
+def simple_upload(request):
+	if request.method == 'POST' and request.FILES['myfile']:
+		myfile = request.FILES['myfile']
+		print("1")
+		fs = FileSystemStorage()
+		filename = fs.save(myfile.name, myfile)
+		uploaded_file_url = fs.url(filename)
+		return render(request, 'info/simple_upload.html',{'uploaded_file_url': uploaded_file_url})
+	return render(request, 'info/simple_upload.html')
